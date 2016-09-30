35626a20986ee3338ebf69b838970fd9d12d62cb
Siddharth <sidn@mit.edu>
2016-09-30 23:52:59 +0200

    adding setup

---
+++ SubMIT/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PANDA_PROD="${EOS2}/pandaprod/v3/" # can have multiple paths, separated by : 
+
+export SUBMIT_CFG="all"
+#export SUBMIT_CFG="test"
+export SUBMIT_LOGDIR="/work/sidn/logs/v4/"
+export SUBMIT_WORKDIR="/work/sidn/submit/"
